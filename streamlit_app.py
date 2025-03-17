fda4dfd69cced1e050d23e63a4b9b08c80018542
jackielim7 <jackieops78@gmail.com>
2025-03-17 14:15:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ def main():
   st.title('Dermatology Machine Learning')  
   st.info('This app using machine learning')
 
+  #input data by user
+  erythema = st.slider('Erythema', min_value=0, max_value=3, value=2)
 if __name__ == "__main__":
   main()
