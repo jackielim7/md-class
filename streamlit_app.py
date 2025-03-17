1c46c857cfc989f0296b5e396ee098f9a19a5d06
jackielim7 <jackieops78@gmail.com>
2025-03-17 14:24:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 import joblib #buat import pickle
 
+def load_model(filename):
+  model = joblib.load(filename)
+  return model
+
+def predict_with_model(model, user_input):
+  prediction = model.predict([user_input])
+  return prediction[0]
+  
+
 def main():
   st.title('Dermatology Machine Learning')  
   st.info('This app using machine learning')
