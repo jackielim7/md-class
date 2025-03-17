a456ccdd8a744bb94a459a8d22f12cb14952531c
jackielim7 <jackieops78@gmail.com>
2025-03-17 14:17:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,6 @@ def main():
 
   #input data by user
   erythema = st.slider('Erythema', min_value=0, max_value=3, value=2) #erythema tulisan buat judul dari slider value itu settingan default pas launch, min nilai slider terkiri, max slider terkanan
+
 if __name__ == "__main__":
   main()
